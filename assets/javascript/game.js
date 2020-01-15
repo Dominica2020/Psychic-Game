0b04ddf60d50ed5444e96be427e1b9c3e76cd9b9
Gecko Uno <dominica.patterson@gmail.com>
2020-01-14 18:26:58 -0800

    Psychic homework phase 3 draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,39 @@
-//List out all the possible letter options [array]
+//List out all the possible letter options [array] & an array holder for wrong letter guesses
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var guesses = [];
+//Create counter variables to hold the number of wins, losses, and guesses left
+var wins = 0;
+var losses = 0;
+var left = 9;
 
 //The computer will choose a random letter out of the list  
-var compGuess = options[Math.floor(Math.random() * options.length)];
+ 
+var compGuess = options[Math.floor(Math.random() * options.length)]; 
 
-//The user will guess a letter by clicking its button on the keyboard. 
-//If a user's letter matches the computer's letter (they win 1 point and the game resets).
-//If the user's letter does not match the computer's letter (the number of guesses left goes down). 
+//Functions
 //Each guess will show in "guesses so far"
 //If the guesses exceed 9 attemps, the user losses and the game resets
 
 document.onkeyup = function() {
   var userGuess = String.fromCharCode(event.keyCode).
   toLowerCase();
-  if (userGuess===compGuess) {
-    alert("Yay!");
+  console.log(userGuess);
+
+//Comparissions
+if (userGuess===compGuess) {
+    wins++;
   }
   else {
-    alert("oops!");
+    left--;
   }
+if (left === 0) {
+  losses++;
 }
 
-//Create variables to hold the number of wins, losses, guesses made, and guesses left
-var wins = 0
-var losses = 0
-var guessesLeft = 9
-var guessesMade
+var html = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + left + "</p>" + "<p>Your Guesses so Far: " + guesses + "</p>"
+document.querySelector("#game").innerHTML = html;
+
+}
+
+//Append letters chosen
+//Reset game after winning & losing
