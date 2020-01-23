fa16b954155f455913e4f8c557d2d8d0c0fdb2ba
Gecko Uno <dominica.patterson@gmail.com>
2020-01-22 19:24:08 -0800

    updated game so that Guesses So Far would be appended and then cleared out after winning or losing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,30 +5,34 @@ var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 var losses = 0;
 var left = 9;
-
+var guesses = "";
 //The computer will choose a random letter out of the list  
- 
-var compGuess = options[Math.floor(Math.random() * options.length)]; 
+
+var compGuess = options[Math.floor(Math.random() * options.length)];
 
 //Functions
-document.onkeyup = function() {
+document.onkeyup = function () {
   var userGuess = String.fromCharCode(event.keyCode).
-  toLowerCase();
-  
-//Comparissions
-if (userGuess===compGuess) {
+    toLowerCase();
+  guesses = guesses + userGuess; //this will append the previous guesses!
+
+
+  //Comparissions
+  if (userGuess === compGuess) {
     wins++;
     left = 9;
+    guesses = "";
   }
   else {
     left--;
   }
-if (left === 0) {
-  losses++;
-  left = 9;
-}
+  if (left === 0) {
+    losses++;
+    left = 9;
+    guesses = "";
+  }
 
-var html = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + left + "</p>" + "<p>Your Guesses so Far: " + userGuess + "</p>"
-document.querySelector("#game").innerHTML = html;
+  var html = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + left + "</p>" + "<p>Your Guesses so Far: " + guesses + "</p>"
+  document.querySelector("#game").innerHTML = html;
 
 }
